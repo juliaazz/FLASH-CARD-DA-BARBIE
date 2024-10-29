32d44bcbbd47fcda07fc7cef719863699a5ff52b
juliaazz <147403379+juliaazz@users.noreply.github.com>
2024-10-29 08:32:56 -0300

    ffff

---
+++ assets/pergunta.js
@@ -0,0 +1,24 @@
+criaCartao(
+    "Qual verdadeiro nome da Barbie?",
+    "Barbara Milliccent Roberts"
+)
+criaCartao(
+    "Quantas profissões a Barbie tem?",
+    "Mais de 180"
+)
+criaCartao(
+    "Quantos animais de estimação ela tem?",
+    "4"
+)
+criaCartao(
+    "Quantos filmes a Barbie tem?",
+    "Mais e 30"
+)
+criaCartao(
+    "Em que ano ela foi lançada",
+    "1959"
+)
+criaCartao(
+    "Qual nome das irmãs da Barbie?",
+    "Skipper,Chelsea,Stacie"
+)
