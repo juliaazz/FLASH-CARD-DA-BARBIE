2f22448cc8b3a1ca46f38da66b58371fbfbcc0c9
juliaazz <147403379+juliaazz@users.noreply.github.com>
2024-11-19 08:38:09 -0300

    .

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,34 @@
 function criaCartao(pergunta, resposta){
-    let conteudo = document.getElementById Conteu
+    let conteudo = document.getElementById ('conteudo')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+
+    cartao.innerHTML = 
+    <div class = "cartao--conteudo">
+    <div class="cartao--pergunta"><p>${pergunta}</p> </div>
+    <div class="cartao--resposta"><p>${resposta}</p></div>
+    </div> 
+    
+    console.log (cartao)
+
+    let respostaEstaVisivel = false 
+    function viracartao () {
+        respostaEstaVisivel = |respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+        console.log ('teste')
+
+    }
+
+    cartao.addEventListener('click', viracartao)
+    conteudo.appendChild(cartao)
+
+
+
+
+
+
+
+
+
+    
 }
